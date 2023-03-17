cd6c7b3eaea405b2906f9645a905d914067edb5c
keya006 <keyanelson006@yahoo.com>
2023-03-16 18:29:11 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- * first c program
+ * main returns 0 
+ * puts outputs he value
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point returns 0
+ * printf - outputs the statement
+ * return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
