0704df89ab077ee4ee857b7e66babfad7131b1d7
keya006 <keyanelson006@yahoo.com>
2023-03-17 15:33:06 -0700

    control structure

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c\n);
+	}
+	return (0);
+}
