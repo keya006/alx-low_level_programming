ea2028d15c89c22bea870be6a7a5836d11e356b5
keya006 <keyanelson006@yahoo.com>
2023-03-19 01:05:26 -0700

    newe

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char d[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", d);
 	return (1);
 }
