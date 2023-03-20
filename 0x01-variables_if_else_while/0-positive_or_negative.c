4ba66bd678aa1a0099f4044a85f83d3ed8f64694
keya006 <keyanelson006@yahoo.com>
2023-03-20 04:49:23 -0700

    newe

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("is positive\n");
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("is zero\n");
 	}
-	else(n < 0)
+	else if (n < 0)
 	{
 		printf("is negative\n");
+	}
 	return (0);
 }
