0c281a5e431a2e586a182f4eecee572e51200131
keya006 <keyanelson006@yahoo.com>
2023-03-23 15:02:33 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - prints a lineon terminal
+ * @n: the parameter of the function
+ *
+ * Return: 0 nothing to return here
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
