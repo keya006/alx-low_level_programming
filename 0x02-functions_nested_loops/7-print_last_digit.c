267c105f2e822b2dd6b7fa14093a654712f9ad04
keya006 <keyanelson006@yahoo.com>
2023-03-23 08:55:55 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
  * print_last_digit - print last digit of no
- * a - parameter 
+ * @a - parameter 
  * Return: value of last digit
  */
 
 int print_last_digit(int a)
 {
-	int c;
-
-	c = a % 10;
-	return (c);
+	return (a % 10);
 }
