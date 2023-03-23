caa8bffc2ee79606cf73ad9e2203e2c2824bc05f
keya006 <keyanelson006@yahoo.com>
2023-03-23 09:11:01 -0700

    sum of a nd b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - sum of two numbers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
