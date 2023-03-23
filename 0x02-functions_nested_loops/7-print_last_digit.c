04056e0905671ee3b5dd3626215bd4d4814065e1
keya006 <keyanelson006@yahoo.com>
2023-03-23 08:32:42 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of no
+ * Return: value of last digit
+ */
+
+int print_last_digit(int a)
+{
+	int a;
+	int b;
+
+	c = a % b;
+	_putchar(c);
+	return (c);
+}
