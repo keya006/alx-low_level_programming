b27241464b04a5b61c59543e9faf064d75eb83ab
keya006 <keyanelson006@yahoo.com>
2023-03-23 06:37:19 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints 10 times
+ *
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char b = 'a';
+	char c = 'z';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (b <= c)
+		{
+			_putchar(b);
+			b++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
