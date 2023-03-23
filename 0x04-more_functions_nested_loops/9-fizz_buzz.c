0075f92a8384867c05a2e78d09747a1e9f967adb
keya006 <keyanelson006@yahoo.com>
2023-03-23 15:29:39 -0700

    fizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i < 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
+
