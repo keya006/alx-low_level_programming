6476d912a9e49204b203a8f37544acefd4150fff
keya006 <keyanelson006@yahoo.com>
2023-03-23 14:37:45 -0700

    product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplication function
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: return product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
