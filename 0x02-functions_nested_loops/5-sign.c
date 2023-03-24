8216b2186a22346ba3bcced9ce6d2a3bd939b424
keya006 <keyanelson006@yahoo.com>
2023-03-24 14:15:27 -0700

    negative positive sign function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - print sign of integer
+ * @n: the integer in question
+ *
+ * Return: 0 or 1 as specified
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
