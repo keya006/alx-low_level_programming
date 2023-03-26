63996422ae339e18d026dd33c02800157b37e196
keya006 <keyanelson006@yahoo.com>
2023-03-26 05:31:29 -0700

    1st 50 fibonacci nos

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry of function
+ *
+ * Return: o denotes success
+ */
+int main(void)
+{
+	int i = 1;
+	int j = 1;
+	int k = 0;
+	int result;
+
+	while (k < 50)
+	{
+		printf("%d, ", j);
+		result = i + j;
+		i = j;
+		j = result;
+		k++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
