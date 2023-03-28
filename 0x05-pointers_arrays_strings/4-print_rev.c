dd2a6a893380c632a696a833de9a7fc2f175871b
keya006 <keyanelson006@yahoo.com>
2023-03-28 10:11:25 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ * @s: the parameter
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '/0'; i++)
+	{
+		count++;
+	}
+	for (i = count; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
