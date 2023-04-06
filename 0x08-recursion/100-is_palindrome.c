80dbaab20d01e9e1bda1343b8357cac5aeeb42d7
keya006 <keyanelson006@yahoo.com>
2023-04-06 16:27:24 -0700

    checks if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if
+ * rev - reverses a string
+ * @s: the string
+ *
+ * Return: returns 1 if true and 0 otherwise
+ */
+
+char rev(*s)
+{
+	if (*s)
+	{
+		return (rev(s + 1));
+	}
+}
+
+
+int is_palindrome(char *s)
+{
+	if (*s == rev(*s))
+	{
+		return (1);
+	}
+	return (0);
+}
