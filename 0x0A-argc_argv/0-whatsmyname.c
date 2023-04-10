247a17b6135359c477c5e232872c98ee867682d4
keya006 <keyanelson006@yahoo.com>
2023-04-10 07:31:38 -0700

    a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - entry point of program
+ * @argc: total number of variables
+ * @argv: the string variables
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (i == 0)
+		{
+			printf("%s\n", argv[0]);
+		}
+	}
+	return (0);
+}
