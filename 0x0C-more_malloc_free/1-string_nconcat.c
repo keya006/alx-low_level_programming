db76b06755c1d8061216685763560be137c1fd90
keya006 <keyanelson006@yahoo.com>
2023-04-14 06:37:33 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,11 +44,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	for (i = 0; i < k; i++)
+	{
 		ptr[i] = s1[i];
-
+	}
 	for (j = 0; j < n; j++)
+	{
 		ptr[i + j] = s2[j];
-
+	}
 	ptr[i + j] = '\0';
 	return (ptr);
 }
