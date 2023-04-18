307cc2e822d52f74797978036c67758aa952ba68
keya006 <keyanelson006@yahoo.com>
2023-04-18 15:23:57 -0700

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifdef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - stores informatin about dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: person responsible for dog
+ *
+ * Description: this is all about dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
