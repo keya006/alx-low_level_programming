9aa42ba34ca5c528dc3dec955c6c498874cbca0c
keya006 <keyanelson006@yahoo.com>
2023-04-19 14:57:13 -0700

    a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,15 @@
 /**
  * print_name - prints a name
  * @name: string to be printed
+ * @f: pointer
  *
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
