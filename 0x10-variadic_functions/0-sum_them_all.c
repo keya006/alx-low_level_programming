ee6b38c7abe9551ec32272f0df4d32a1a9b1048e
keya006 <keyanelson006@yahoo.com>
2023-04-22 11:31:00 -0700

    variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: the number of parameters
+ *
+ * Return: sum of args
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+
+	int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		var_list ptr;
+		var_start (ptr, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += var_arg(ptr, int);
+		}
+		var_end(ptr);
+		return (sum);
+	}
+	return (0);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This repo deals with variadic functions, how to use va_start, va_arg and va_end macros and how to use the const type qualifier
